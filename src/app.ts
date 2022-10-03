74816e61a9f4127be38682e0f3d3f39278165019
Krystian Gawlik <kgawlik@future-processing.com>
2022-10-03 11:34:42 +0200

    boilerplate

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser);
+
+app.listen(3000, () =>{
+    console.log('Server is running...'); 
+});
