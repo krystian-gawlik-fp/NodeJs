b925505c1bc97b1fefed95c21d53b9a11383315a
Krystian Gawlik <kgawlik@future-processing.com>
2022-10-11 12:46:07 +0200

    Added docker and Postgres

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,18 @@ import bodyParser from 'body-parser'
 import { RegisterRoutes } from './routes/routes'
 import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from './swagger.json'
+import db from './util/database'
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 app.use(bodyParser.json())
 RegisterRoutes(app)
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.listen(3000, () => {
-  console.log('Server is running...')
+db.query('CREATE TABLE IF NOT EXISTS test (value int);')
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Server is running on port ${process.env.APP_PORT}...`)
 })
--- src/controllers/version.ts
+++ src/controllers/version.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Route } from 'tsoa'
+import { Controller, Get, Route, Tags } from 'tsoa'
 import { GetVersionResponse } from '../models/getVersionResponse'
 import { execSync } from 'child_process'
 
+@Tags('version')
 @Route('version')
 export class Version extends Controller {
   @Get()
